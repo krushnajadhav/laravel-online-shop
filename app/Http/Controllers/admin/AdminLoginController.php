2b2b9700b54d98e5823ad8c5eb2a948b19f61f5d
krushna <jadhavkrushna025@gmail.com>
2023-09-02 16:16:51 +0530

    my commit

--- app/Http/Controllers/admin/AdminLoginController.php
+++ app/Http/Controllers/admin/AdminLoginController.php
@@ -1,11 +1,37 @@
 <?php
 
 namespace App\Http\Controllers\admin;
+use Illuminate\Support\Facades\Validator;
+
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class AdminLoginController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('admin.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+
+        $validator = Validator::make($request->all(),[
+            'email'=>'required|email',
+            'password' => 'required'
+        ]);
+
+        if($validator->passes())
+        {
+
+        }
+        else
+        {
+            return redirect()->route('admin.login')
+            ->withErrors($validator)
+            ->withInput($request->only('email'));
+        }
+
+    }
 }
